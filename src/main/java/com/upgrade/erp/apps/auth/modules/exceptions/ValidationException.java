641c2fe971f23837dbed213921dda57f011e2a8f
Jair Lenin Lucas Benavides <jairlucasbe@gmail.com>
2025-01-23 14:15:45 -0500

    directorio acomodado para mvc

--- src/main/java/com/upgrade/erp/apps/auth/modules/exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.upgrade.erp.apps.auth.modules.exceptions;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/upgrade/erp/server/entities/RoleEnum.java
@@ -0,0 +1,9 @@
+package com.upgrade.erp.server.entities;
+// package com.upgrade.erp.modules.auth.persistence.entities;
+
+// public enum RoleEnum {
+//     ADMIN,
+//     USER,
+//     INVITED,
+//     DEVELOPER
+// }

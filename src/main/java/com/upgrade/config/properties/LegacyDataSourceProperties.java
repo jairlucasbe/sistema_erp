3d85e33b57366b68d8e36b8e6f8ba3458f8b7638
Jair Lenin Lucas Benavides <jairlucasbe@gmail.com>
2025-01-25 16:23:19 -0500

    Properties Config initialized with legacyDatasource

---
+++ src/main/java/com/upgrade/config/properties/LegacyDataSourceProperties.java
@@ -0,0 +1,64 @@
+package com.upgrade.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "legacy.datasource")
+public class LegacyDataSourceProperties {
+
+    private String url;
+    private String username;
+    private String password;
+    private String driverClassName;
+    private boolean showSql;
+    private String hibernateDialect;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public boolean isShowSql() {
+        return showSql;
+    }
+
+    public void setShowSql(boolean showSql) {
+        this.showSql = showSql;
+    }
+
+    public String getHibernateDialect() {
+        return hibernateDialect;
+    }
+
+    public void setHibernateDialect(String hibernateDialect) {
+        this.hibernateDialect = hibernateDialect;
+    }
+}
